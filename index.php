f48aa35b114e03e37b7746afa9e8f89b9b3d7965
mdiamo <mdiamo@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-11 02:57:24 +0000

    update
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@68 6123cefa-f26d-4250-b61a-f760f7026737

--- index.php
+++ index.php
@@ -9,57 +9,43 @@
     <head>
         <title>TRIP OUT!</title>    
         <link rel ="stylesheet" type ="text/css" href ="css/bootstrap.css">
-        <style type ="text/css">
-            body{
-                padding-top: 60px;
-                padding-bottom: 40px;
-            }
-        </style>
         <link rel ="stylesheet" type ="text/css" href ="css/bootstrap-responsive.css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     </head>
+
     <body> 
         <script src="js/bootstrap.js"></script>
         <script src="js/script.js"></script>
         <div class ="container">
-            <div class ="row">
-                TRIP OUT!
-                <div class= "pull-right">
+            <div class ="row" style ="background-color:black;">
+                <img src="media/assets/images/tripOutLogo.jpg"></img>
+                <div class ="pull-right" style="padding-top:20px;">
                     Login: <input type="text" placeholder="username" name="tag" value="" />
                     <input type="password" placeholder="password" name="tag" value="" />
+                    <br>
+                    <a href="">Sign Up</a> 
+                    <a href="" style="align:right;">forgot password?</a>
                 </div>
             </div>
-            
-            <div class="row">
-                <div class="navbar">
-                    <div class="navbar-inner">
-                        <div class="container">
-                            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </a>
-                            <div class="nav-collapse">
-                                <ul class="nav">
-                                    <li class="active"><a href="#" id ="homeLink"> <span class="glyphicon glyphicon-home"></span> Home</a></li>
-                                    <li><a href="#" id ="aboutLink">About</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                </ul>
-                            </div>
-                            <!--/.nav-collapse -->
-                        </div>
-                    </div>
+            <nav class="navbar navbar-inverse" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Write a Review</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </nav>
+            <div class="panel panel-default">
+                <div class="panel-heading">Plan Your Destination</div>
+                <div class="panel-body">
+                  <form name="citySearch" method ="GET" action="city_search.php">
+                        <input type="text" class="search-query" placeholder="Enter search keywords" name="tag" value="" />
+                        <button type="submit" class="btn btn-default" value="Go">Search</button>
+                    </form>
                 </div>
             </div>
         </div>
-            <div class="container" id="mainContent">
-                <form name="citySearch" method ="GET" action="city_search.php">
-                    Enter keywords:
-                    <input type="text" class="search-query" placeholder="Search" name="tag" value="" />
-                    <button type="submit" class="btn btn-default" value="Go">Search Destinations</button>
-                </form>
-            </div>
     </body>
 </html>
