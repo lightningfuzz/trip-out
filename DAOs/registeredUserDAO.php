4c11a364d888e8e43a1d3e611bfb57f31ff53328
xshum <xshum@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-11 21:43:47 +0000

    1. updated registeredUserDAO with more functions implemented 2. updated registeredUser
    -deon
    
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@86 6123cefa-f26d-4250-b61a-f760f7026737

--- DAOs/registeredUserDAO.php
+++ DAOs/registeredUserDAO.php
@@ -1,20 +1,13 @@
 <?php
+
 require_once("../dbConnect.php");
 
 //singleton class
-class registeredUserDAO {
+class RegisteredUserDAO {
 
     //private construct
-    private function __construct() {  
-    }
-
-    private static $instance = null;
-    
-    public static function getInstance() {
-        if (!self::$instance instanceof self) {
-            self::$instance = new self;
-        }
-        return self::$instance;
+    private function __construct() {
+        
     }
 
     /**
@@ -22,20 +15,62 @@ class registeredUserDAO {
      * @param $user is 
      * @return
      */
-    public function create(registeredUser $user) {
+    public static function create(RegisteredUser $user) {
 
         $db = dbConnect::getInstance();
         //query
         $q = "INSERT INTO registered_user ('user_name', 'password', 'reg_time', "
-                . "'last_login_time', 'email', 'num_reviews') VALUES ('" 
+                . "'last_login_time', 'email', 'num_reviews') VALUES ('"
                 . $user->getUserName() . "', '" . $user->getPassword() . "', '"
                 . $user->getRegTime() . "', '" . $user->getLastLoginTime() . "', '"
-                . $user->getEmail() . "', '" . $user->getNumReviews(). "')";
+                . $user->getEmail() . "', '" . $user->getNumReviews() . "')";
 
-        $result = $db->query($q);
+        $db->query($q);
         $user->setUserID($db->insert_id);
+
         return $user;
     }
+
+    /**
+     * returns an RegisteredUser object
+     * @param $id the user id
+     * @return a RegisteredUser object
+     */
+    public static function getByID($id) {
+
+        $db = dbConnect::getInstance();
+        $ru = new RegisteredUser();
+        //query
+        $q = "SELECT * FROM registered_user WHERE user_id =" . $id;
+
+        if (!$result = $db->query($q)) return null;
+
+        $var = mysqli_fetch_array($result);
+        $ru->setUserID($var["user_id"]);
+        $ru->setUserName($var["user_name"]);
+        $ru->setPassword($var["password"]);
+        $ru->setRegTime($var["reg_time"]);
+        $ru->setLastLoginTime($var["last_login_time"]);
+        $ru->setEmail($var["email"]);
+        $ru->setNumReviews($var["num_reviews"]);
+        
+        return $ru;
+    }
+    
+    public static function update(RegisteredUser $user){
+
+        $db = dbConnect::getInstance();
+        //query
+        $q = "UPDATE registered_user SET  ('user_name' = " . $user->getUserName() . ", 'password' = " . $user->getPassword() 
+            . "'reg_time' =  " . $user->getRegTime() . ", 'last_login_time' = " . $user->getLastLoginTime()
+                . "'email' = " . $user->getEmail() . ", 'num_reviews' = " . $user->getNumReviews()
+                . ") WHERE 'user_id = " . $user->getUserID() . "'";
+
+        if(!$db->query($q)) return null;
+
+        return $user;
+    }
+
 }
 
 ?>
--- Models/registeredUser.php
+++ Models/registeredUser.php
@@ -1,6 +1,6 @@
 <?php
 
-class registeredUser {
+class RegisteredUser {
 
     private $userName;
     private $password;
