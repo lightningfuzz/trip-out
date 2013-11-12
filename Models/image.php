7cd3024f32c18e59780f9ee29ba758c50be14b50
jkim <jkim@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-12 00:18:59 +0000

    The updates has been posted: - class files except keyword and tag added on model directory.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@92 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ Models/image.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of image
+ *
+ * @author Jung Hwan Kim
+ */
+class Image extends Media{
+    //put your code here
+}
+
+?>
---
+++ Models/video.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of video
+ *
+ * @author Jung Hwan Kim
+ */
+class Video extends Media {
+    //put your code here
+}
+
+?>
