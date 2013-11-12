deb7273981d53fd8583d321dca5f9163808f61f8
akhine <akhine@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-12 12:08:50 +0000

    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@108 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ Views/script.js
@@ -0,0 +1,35 @@
+$id ='';
+$(document).ready( function() {
+    //$("#mainContent").load('pages/main.html')
+    $("#aboutLink").on("click", function() {
+        $("#mainContent").load('pages/about.html');
+    });
+    
+    $("#homeLink").on("click", function() {
+        $("#mainContent").load('pages/main.html');
+    });
+    $("#signInUpLink").on("click", function() {
+        $("#mainContent").load('signIn.php');
+    });
+    
+    $("#registerButton").on("click", function() {
+        $("#mainContent").load('createNewUser.php');
+    });
+    
+    $("#editReviewLink").on("click", function() {
+        $("#mainContent").load('editReviewList.php');
+    });
+    
+    //todo destroy user cookie on logout!
+    $("#logoutLink").on("click", function() {
+    });
+    
+    
+    /*$("form").submit(function() {
+        $.post($(this).attr("action"), $(this).serialize(), function(data) {
+            $("#mainContent").html(data);
+        });
+        return false; // prevent normal submit
+    });
+    */
+});
