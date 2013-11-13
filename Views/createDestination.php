a67062e13ab99621fa299429fda464f650f8235f
rpenno <rpenno@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-13 03:51:42 +0000

    added top bar to create destination
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@120 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/createDestination.php
+++ Views/createDestination.php
@@ -17,6 +17,29 @@ and open the template in the editor.
         <title>Create a Destination</title>
     </head>
     <body>
+        <div style ="background-color:black;">
+                <img src="media/assets/images/tripOutLogo.jpg"></img>
+                <div class ="pull-right" style="padding-top:20px;color:white;">
+                    Login: <input type="text" placeholder="username" name="tag" value="" />
+                    <input type="password" placeholder="password" name="tag" value="" />
+                    <br>
+                        <div class="col-md-2" style="padding-top:5px;">
+                            <button class ="btn btn-primary">Sign Up</button>
+                        </div>
+                        <div style = "text-align:right; float:right;padding-top:10px;padding-right:10px;">
+                            <a href="" >forgot password?</a>
+                        </div>
+                </div>
+            </div>
+            <nav class="navbar navbar-inverse" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Write a Review</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </nav>
         <h2 align="center">Create a Destination</h2>
         <div align="center">
              <div style="border:2px solid; border-radius:25px; display: block;
