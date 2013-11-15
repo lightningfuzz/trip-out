ec1b8d2f2f81e34dbe10af0248cc1ce6d3a54708
xshum <xshum@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-15 05:08:54 +0000

    added tagDAO.php
    -deon
    
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@136 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ DAOs/tagDAO.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of tagDAO
+ *
+ * @author deon
+ */
+require_once("../dbConnect.php");
+
+class TagDAO {
+
+    //private construct
+    private function __construct() {
+        
+    }
+
+    /**
+     * creates a new user account; returns a new user id.
+     * @param $user is the new RegisteredUser object
+     * @return the $user object with a new user id; return null if failed
+     */
+    public static function create(Tag $tag) {
+
+        $db = dbConnect::getInstance();
+        //query
+        $q = "INSERT INTO tag (dest_id, keyword_id) VALUES ('"
+                . $tag->getDestId() . $tag->getKeywordId() . "')";
+
+        if (!$db->query($q)) return null;
+
+        return $tag;
+    }
+
+    /**
+     * searches the registered_user table by id
+     * @param $id the user id
+     * @return a RegisteredUser object if found; else return null
+     */
+    public static function getByKeywordID($id) {
+
+        $db = dbConnect::getInstance();
+        //query
+        $q = "SELECT * FROM tag WHERE keyword_id =" . $id;
+
+        if (!$result = $db->query($q)) return null;
+        
+        $arr = mysqli_fetch_array($result);
+        $tag = new Tag();
+        $tag->setDestId($arr["dest_id"]);
+        $tag->setKeywordId($arr["keyword_id"]);
+        
+        return $tag;
+    }
+}
