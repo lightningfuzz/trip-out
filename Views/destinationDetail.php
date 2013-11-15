5c9f37c6cc96b866797367ade0883bc651a72160
mruthe <mruthe@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-15 16:54:49 +0000

    Created basic review page and fixed some bugs on destination detail
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@138 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/destinationDetail.php
+++ Views/destinationDetail.php
@@ -1,21 +1,31 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <!DOCTYPE HTML>
 <html>
 	<head>
             <title>Detail Trip</title> 
-		<link href="DestinationCSS.css" type="type/css" rel="stylesheet" media="all" />
-                <link rel ="stylesheet" type ="text/css" href ="http://sfsuswe.com/~mruthe/css/bootstrap.css">
-                <link rel ="stylesheet" type ="text/css" href ="http://sfsuswe.com/~mruthe/css/bootstrap-responsive.css"> 
+		<link rel ="stylesheet" type="type/css" href ="../css/destinationDetail.css" /> 
+                <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css">
+                <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css"> 
+                
+                        <!--FANCY BOX FILES-->
+                <!-- Add jQuery library -->
+                <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+                <!-- Add mousewheel plugin (this is optional) -->
+                <script type="text/javascript" src="../fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+                <!-- Add fancyBox -->
+                <link rel="stylesheet" href="../fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+                <script type="text/javascript" src="../fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+                <!-- Optionally add helpers - button, thumbnail and/or media -->
+                <link rel="stylesheet" href="../fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+                <script type="text/javascript" src="../fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+                <script type="text/javascript" src="../fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+                <link rel="stylesheet" href="../fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+                <script type="text/javascript" src="../fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+                <!--END FANCYBOX FILES-->
 	</head>
 	<body>
-            <script src="http://sfsuswe.com/~mruthe/js/bootstrap.js"></script>
-            <script src="http://sfsuswe.com/~mruthe/js/script.js"></script>
+            <script src="../js/bootstrap.js"></script>
+            <script src="../js/script.js"></script>
             <div class ="container">
             <div style ="background-color:black;">
                 <img src="http://sfsuswe.com/~mruthe/media/assets/images/tripOutLogo.jpg"></img>
@@ -40,22 +50,32 @@
                     <li><a href="#">FAQ</a></li>
                 </ul>
             </nav>
-            </div>
+            
 		<div>
 		<div id = 'photo'>
-			<p>Photo</p>
-			<img src="http://sfsuswe.com/~mruthe/media/images/dumplingking.jpg"/>
+                    <a href="../media/images/dumplingking.jpg">
+                            <img src="../media/images/dumplingking.jpg" alt="" class="img-thumbnail" height="140px" width ="140px">
+                    </a>
+                    <!--<a class="fancybox" href="../media/images/dumplingking.jpg" data-fancybox-group="gallery" title="Lorem ipsum dolor sit amet"><img src="1_s.jpg" alt="" /></a>-->
+                    <!--<a id="single_image" href=""><img src="../media/images/dumplingking.jpg"/></a>-->
 		</div>
 		<div id = 'info'>
-			<h2>Destination Name</h2>
-			<p name="pop">Category</p>
-			<p id = 'pop'>Description</p>
-			<p name = 'pop'>Website URL</p>
+			<h2>Shanghai Dumpling King</h2>
+			<p name="pop">Restaurant / Chineese</p>
+			<p id = 'pop'>Food to go Call 387-2088</p>
+			<p name = 'pop'><a  href="http://www.shanghaidumplingkingsf.com/">Website</a></p>
 		</div>
 		</div>
-			<p > More Images </p>
+			<p><a href ="../Views/review.php"> Upload Media </a></p>
+                        <p><a href="../Views/mediaViewer.php">More media of Shanghai Dumpling King</a></p>
                 <div>
-			<p>Reviews</p>
+                    <h4>Reviews</h4>
+                    <ul>
+                        <li><p>Good Eats!</p>
+                            <p>This was the best food I have ever eaten ever.</p>
+                        </li>
+                    </ul>
+                </div>
                 </div>
 	</body>
 </html>
--- Views/review.php
+++ Views/review.php
@@ -1,7 +1,28 @@
-<?php
+<html>
+    <head>
+        <title>TRIP OUT!</title>    
+        <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css">
+        <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css">
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/script.js"></script>
+        <script src="../js/jquery.js"></script>
+        <!-- Add jQuery library -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    </head>
+    <div class="container">
+     <div class="row">
+         <div class="span5">
+                <h3>Write a Review</h3>
+                <p>
+                <form name="logon" action="index.php" method="POST" >
+                    Title: <input type="text" name="user"><br>
+                    Review: <input type="password" name="userpassword"><br>
+                    Upload Media: <input class="btn btn-default" type="submit" id="editButton" value="Browse">
+                    <input class="btn btn-default" type="submit" id="editButton" value="Submit">
+                </form>
+                </p>
+         </div>
+     </div>
+    </div>
+</html>
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
