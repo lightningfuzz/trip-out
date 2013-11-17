5a0ed1b22bc0971a5ce71e636d5e68e2b1c27911
akhine <akhine@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-17 23:18:59 +0000

    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@191 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/searchResult.php
+++ Views/searchResult.php
@@ -1,83 +1,88 @@
-<?php
-function getValue($pname) {
-        if(isset($_POST[$pname])){
-		$pvalue = $_POST[$pname];
+<?php /*
+function getValue($_GET['searchString']) {
+        if(isset($_GET['searchString']){
+		$pvalue = $_GET['searchString'];
 	} else {
 		$pvalue = "";
 	}
         return $pvalue;
 }
-
 $searchVal = getValue("txtSearch");
 $searchTp = getValue("searchType");
+   */ ?>
 
-	
-    ?>
+
+<?php
+
+require_once '../Controllers/SearchController.php';
+$search = new Search($_GET['searchString']);
+$result = $search->run();
+
+?>
+
 <html>
     <head>
         <title>TRIP OUT!</title>    
         <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css">
         <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css">
+        <link rel ="stylesheet" type ="text/css" href ="css/index.css">
+        <link rel = "stylesheet" type = "text/css" href = "../css/searchResult.css">
+        <script src="js/bootstrap.js"></script>
+        <script src="js/script.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel = "stylesheet" type = "text/css" href = "../css/searchResult.css">
+	
     </head>
 
     <body> 
-        <script src="../js/bootstrap.js"></script>
-        <script src="../js/script.js"></script>
-        <div class ="container">
-            <div style ="background-color:black;">
-                <img src="../media/assets/images/tripOutLogo.jpg"></img>
-                <div class ="pull-right" style="padding-top:20px;color:white;">
-                    Login: <input type="text" placeholder="username" name="tag" value="" />
-                    <input type="password" placeholder="password" name="tag" value="" />
-                    <br>
-                        <div class="col-md-2" style="padding-top:5px;">
-                            <button class ="btn btn-primary">Sign Up</button>
-                        </div>
-                        <div style = "text-align:right; float:right;padding-top:10px;padding-right:10px;">
-                            <a href="" >forgot password?</a>
-                        </div>
-                </div>
-            </div>
+         <div class ="container">
             <nav class="navbar navbar-inverse" role="navigation">
+                <a class="navbar-brand" href="#">TRIP OUT!</a>
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="Views/reviewSearch.php">Write a Review</a></li>
+                    <li><a href="Views/about.html">About</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Write a Review</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
+                <form class="navbar-form navbar-right">
+                    <a type="submit" class="btn btn-default" href="Views/signIn.php">Sign In</a>
+                    <a type="submit" class="btn btn-default" href="Views/signUp.php">Register</a>
+                </form>
             </nav>
+            <div class ="mainContent">
+                <div class ="overview">
+                    <h1>Welcome to Trip Out!</h1>
+                    Plan your trip, read reviews, share your experiences
+                </div>
             
             <div class="panel panel-default" style="width:75%;margin-left:auto;margin-right:auto;">
                 <div class="panel-heading">Plan Your Trip </div>
                 <div class="panel-body" align="center">
-                    <form action="searchResult.php" method="post" class="form-inline" role="form">
+                    <form action="searchResult.php" method="get" class="form-inline" role="form">
                     <div style="padding-bottom:10px;">
                         
                         <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox5" name = "searchType" value="option5" <?php if($searchTp == "option5") echo "checked"; ?>> All
+                            <input type="radio" id="inlineCheckbox5" name = "type" value = "0" <?php if($_GET['type'] == "0") echo "checked"; ?>> All
                         </label>
                         <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox1" name ="searchType" value="option1" <?php if($searchTp == "option1") echo "checked"; ?> > Attractions
+                            <input type="radio" id="inlineCheckbox1" name = "type" value = "1" <?php if($_GET['type'] == "1") echo "checked"; ?> > Attractions
                         </label>
                         <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox2" name ="searchType" value="option2" <?php if($searchTp == "option2") echo "checked"; ?>> Restaurants
+                            <input type="radio" id="inlineCheckbox2" name = "type" value = "2" <?php if($_GET['type'] == "2") echo "checked"; ?>> Restaurants
                         </label>
                         <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox3" name = "searchType" value="option3" <?php if($searchTp == "option3") echo "checked"; ?>> Hotels
+                            <input type="radio" id="inlineCheckbox3" name = "type" value = "3" <?php if($_GET['type'] == "3") echo "checked"; ?>> Hotels
                             
                         </label>
                         <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox4" name = "searchType" value="option4" <?php if($searchTp == "option4") echo "checked"; ?>> Events
+                            <input type="radio" id="inlineCheckbox4" name = "type" value = "4" <?php if($_GET['type'] == "4") echo "checked"; ?>> Events
                         </label>
                         
                     </div>
                     
                         <div class="form-group" style="width:40%;">
-                          <input name="txtSearch" class="form-control"  placeholder="Enter search keywords" value="<?php echo $searchVal; ?>">
+                          <input name="searchString" class="form-control"  placeholder="Enter search keywords" value= "<?php echo $_GET['searchString']; ?>">
                         </div>
                         <button type="submit" class="btn btn-default">Search Destinations</button>
                     </form>
@@ -87,7 +92,7 @@ $searchTp = getValue("searchType");
             <!-- Filter By (left_pane)
             
             <div class = "left_pane">
-			<h4> Filter By <?php echo $searchVal;?>: </h4>			
+			<h4> Filter By <?php /*echo $searchVal;*/?>: </h4>			
 				
 			<input type = "checkbox" name = "filter" value = "Landmarks"> Landmarks <br/>
 			<input type = "checkbox" name = "filter" value = "Museums"> Museums <br/>
@@ -100,119 +105,41 @@ $searchTp = getValue("searchType");
             -->
 		<div class = "content">
                     
-			<h3 style = "color: green"> Search Results for "<?php echo $searchVal;?>" </h3> 
+			<h3 style = "color: green"> Search Results for "<?php echo $_GET['searchString']; ?>" </h3> 
 			<p style = "text-align: right; padding-right: 200px"> Showing 1 - 5 out of 10 </p> <br/>
-			<div class = "destination">
-                                <div class ="search_name"> 
-				<img src = "../media/images/landmark1.jpg" width = "150" height = "100" style = "float: left; margin: 5px;">			
-				<a href = "destinationDetail.php" style = "text-decoration: none"> 			
-					<h4> (1) Marina Bay Sands </h4> 
-				</a>
-                                 <img src="../media/images/rate3.png"/><img src="../media/images/rate1.png"/>
-                                 <img src="../media/images/rate1.png"/><img src="../media/images/rate1.png"/>
-                                 <img src="../media/images/rate1.png"/> 10 reviews<br/>
-				&nbsp; Landmarks
-                            </div>
-                            <div class ="search_addr">
-                                10 Bayfront Avenue <br/>
-                                Singapore 018956 <br/>
-                                +65 6688 8868
-                            </div>
-				<p style = "float: left; clear: both">
-					Marina Bay Sands is newly built last three years ago and became one of the landmarks of Singapore! 
-				</p>					
-			</div>
-
-			<div class = "destination">
-                                <div class ="search_name"> 
-				<img src = "../media/images/twinTower.jpg" width = "150" height = "100" style = "float: left; margin: 5px;">			
-				<a href = "destinationDetail.php" style = "text-decoration: none"> 			
-					<h4> (2) Twin Tower </h4> 
-				</a>
-                                 <img src="../media/images/rate3.png"/><img src="../media/images/rate3.png"/>
-                                 <img src="../media/images/rate3.png"/><img src="../media/images/rate1.png"/>
-                                 <img src="../media/images/rate1.png"/> 43 reviews<br/>
-				&nbsp; Landmarks
-                            </div>
-                                        <div class ="search_addr">
-                                          10 Bayfront Avenue <br/>
-                                          Singapore 018956 <br/>
-                                          +65 6688 8868
-                                        </div>
-					<p style = "float: left; clear: both">
-						Twin Tower is one of the most famous landmarks in Malaysia! 
-					</p>			
-			</div>
-
-			<div class = "destination">
-                                <div class ="search_name"> 
-				<img src = "../media/images/landmark4.jpg" width = "150" height = "100" style = "float: left; margin: 5px;">			
-				<a href = "destinationDetail.php" style = "text-decoration: none"> 			
-					<h4> (3) Esplanade </h4> 
-				</a>
-                                 <img src="../media/images/rate3.png"/><img src="../media/images/rate3.png"/>
-                                 <img src="../media/images/rate2.png"/><img src="../media/images/rate1.png"/>
-                                 <img src="../media/images/rate1.png"/> 12 reviews<br/>
-				&nbsp; Landmarks
-                            </div>
-                                        <div class ="search_addr">
-                                          10 Bayfront Avenue <br/>
-                                          Singapore 018956 <br/>
-                                          +65 6688 8868
-                                        </div>
-					<p style = "float: left; clear: both">
-						Twin Tower is one of the most famous landmarks in Malaysia! 
-					</p>			
-			</div>
+			
                         
+                        <?php 
+                             $no = 1;
+                             foreach($result as $dest):
+                             $dest . "<br>";
+                             
+                        ?>
                         <div class = "destination">
                                 <div class ="search_name"> 
-				<img src = "../media/images/twinTower.jpg" width = "150" height = "100" style = "float: left; margin: 5px;">			
+                                    <img src = <?php echo $dest->getImageUrl(); ?>  width = "150" height = "100" style = "float: left; margin: 5px;">			
 				<a href = "destinationDetail.php" style = "text-decoration: none"> 			
-					<h4> (4) Twin Tower </h4> 
-				</a>
-                                 <img src="../media/images/rate3.png"/><img src="../media/images/rate2.png"/>
-                                 <img src="../media/images/rate1.png"/><img src="../media/images/rate1.png"/>
-                                 <img src="../media/images/rate1.png"/> 6 reviews<br/>
-				&nbsp; Landmarks
-                            </div>
-                                        <div class ="search_addr">
-                                          10 Bayfront Avenue <br/>
-                                          Singapore 018956 <br/>
-                                          +65 6688 8868
-                                        </div>
-					<p style = "float: left; clear: both">
-						Twin Tower is one of the most famous landmarks in Malaysia! 
-					</p>			
-			</div>
-                        
-                        <div class = "destination">
-                                <div class ="search_name"> 
-				<img src = "../media/images/twinTower.jpg" width = "150" height = "100" style = "float: left; margin: 5px;">			
-				<a href = "destinationDetail.php" style = "text-decoration: none"> 			
-					<h4> (5) Twin Tower </h4> 
+                                    <h4> (<?php echo $no++; ?>) <?php echo $dest->getName(); ?> </h4> 
 				</a>
                                  <img src="../media/images/rate3.png"/><img src="../media/images/rate3.png"/>
                                  <img src="../media/images/rate3.png"/><img src="../media/images/rate3.png"/>
                                  <img src="../media/images/rate2.png"/> 55 reviews<br/>
-				&nbsp; Landmarks
+				
                             </div>
                                         <div class ="search_addr">
-                                          10 Bayfront Avenue <br/>
-                                          Singapore 018956 <br/>
-                                          +65 6688 8868
+                                          <?php echo $dest->getAddress(); ?>
                                         </div>
 					<p style = "float: left; clear: both">
-						Twin Tower is one of the most famous landmarks in Malaysia! 
+						<?php echo $dest->getDescription(); ?>
 					</p>			
 			</div>
-                        
+                        <?php endforeach; ?>
 
 		
 
        <!-- Can't find the destination? Create a new Destination -->
        
-       <form action="createDestination.php" method="post" class="form-inline" role="form">
+       <form action="createDestination.php" method="post" class="form-inline">
        <div class ="newDest">
            <p>
                Can't find your destination? Create a new one!
