da927b03a7d6f044c3c0a8ac25063d6fe8846e86
msante <msante@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-17 21:47:09 +0000

    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@181 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ Models/Image.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Description of image
+ *
+ * @author Jung Hwan Kim
+ */
+class Image extends Media{
+    
+   
+    
+}
+
+?>
---
+++ Models/Video.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of video
+ *
+ * @author Jung Hwan Kim
+ */
+class Video extends Media {
+    //put your code here
+}
+
+?>
