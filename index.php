dd50153c8564b7a1e85a6b39ae45ec2fff1cdbff
mdiamo <mdiamo@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-17 21:54:57 +0000

    Reformatted nav bar.   Added footer.  Added overview statement on index.  Added links to about, write a review, sign in/up.  created index.css which contains footer formatting.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@183 6123cefa-f26d-4250-b61a-f760f7026737

--- index.php
+++ index.php
@@ -1,69 +1,59 @@
 <?php
 
-	
-    ?>
+?>
 <html>
     <head>
         <title>TRIP OUT!</title>    
         <link rel ="stylesheet" type ="text/css" href ="css/bootstrap.css">
         <link rel ="stylesheet" type ="text/css" href ="css/bootstrap-responsive.css">
+        <link rel ="stylesheet" type ="text/css" href ="css/index.css">
+        <script src="js/bootstrap.js"></script>
+        <script src="js/script.js"></script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
-
     <body> 
-        <script src="js/bootstrap.js"></script>
-        <script src="js/script.js"></script>
         <div class ="container">
-            <div style ="background-color:black;">
-                <img src="media/assets/images/tripOutLogo.jpg"></img>
-                <div class ="pull-right" style="padding-top:20px;color:white;">
-                    Login: <input type="text" placeholder="username" name="tag" value="" />
-                    <input type="password" placeholder="password" name="tag" value="" />
-                    <br>
-                        <div class="col-md-2" style="padding-top:5px;">
-                            <button class ="btn btn-primary">Sign Up</button>
-                        </div>
-                        <div style = "text-align:right; float:right;padding-top:10px;padding-right:10px;">
-                            <a href="" >forgot password?</a>
-                        </div>
-                </div>
-            </div>
             <nav class="navbar navbar-inverse" role="navigation">
+                <a class="navbar-brand" href="#">TRIP OUT!</a>
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="Views/reviewSearch.php">Write a Review</a></li>
+                    <li><a href="Views/about.html">About</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Write a Review</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
+                <form class="navbar-form navbar-right">
+                    <a type="submit" class="btn btn-default" href="Views/signIn.php">Sign In</a>
+                    <a type="submit" class="btn btn-default" href="Views/signUp.php">Register</a>
+                </form>
             </nav>
-            
+            <div class ="overview">
+                <h1>Welcome to Trip Out!</h1>
+                Plan your trip, read reviews, share your experiences
+            </div>
             <div class="panel panel-default" style="width:75%;margin-left:auto;margin-right:auto;">
                 <div class="panel-heading">Plan Your Trip</div>
                 <div class="panel-body" align="center">
-                     <form action="Views/searchResult.php" method="post" class="form-inline" role="form">
-                    <div style="padding-bottom:10px;">
-                        
-                        <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox5" name = "searchType" value="option5"> All
-                        </label>
-                        <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox1" name ="searchType" value="option1"> Attractions
-                        </label>
-                        <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox2" name ="searchType" value="option2"> Restaurants
-                        </label>
-                        <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox3" name = "searchType" value="option3"> Hotels
-                            
-                        </label>
-                        <label class="checkbox-inline">
-                            <input type="radio" id="inlineCheckbox4" name = "searchType" value="option4"> Events
-                        </label>
-                        
-                    </div>
-                   
+                    <form action="Views/searchResult.php" method="post" class="form-inline" role="form">
+                        <div style="padding-bottom:10px;">
+                            <label class="checkbox-inline">
+                                <input type="radio" id="inlineCheckbox5" name = "searchType" value="option5"> All
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="radio" id="inlineCheckbox1" name ="searchType" value="option1"> Attractions
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="radio" id="inlineCheckbox2" name ="searchType" value="option2"> Restaurants
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="radio" id="inlineCheckbox3" name = "searchType" value="option3"> Hotels
+
+                            </label>
+                            <label class="checkbox-inline">
+                                <input type="radio" id="inlineCheckbox4" name = "searchType" value="option4"> Events
+                            </label>
+                        </div>
                         <div class="form-group" style="width:40%;">
                           <input name="txtSearch" class="form-control"  placeholder="Enter search keywords" value="">
                         </div>
@@ -71,8 +61,11 @@
                     </form>
                 </div>
             </div>
-            Promotions
-
+            <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+                <div class ="footer">
+                    SFSU-FAU-FULDA joint SW Engineering Project Fall 2013
+                </div>
+            </nav>
         </div>
     </body>
 </html>
