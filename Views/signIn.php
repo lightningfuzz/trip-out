27e3803654a7e31d2a98d6ad6ce632002cbaac63
mdiamo <mdiamo@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-17 22:26:13 +0000

    Added navbar and footers to sign in and sign up.  Implemented links
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@185 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/signIn.php
+++ Views/signIn.php
@@ -1,9 +1,10 @@
 <html>
     <head>
-        <title>TRIP OUT!</title>    
+        <title>TRIP OUT! - Sign In</title>    
         <!--<link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css"> -->
         <link rel ="stylesheet" type ="text/css" href ="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
         <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css">
+        <link rel ="stylesheet" type ="text/css" href ="../css/index.css">
         <script src="../js/bootstrap.js"></script>
         <script src="../js/script.js"></script>
         <script src="../js/jquery.js"></script>
@@ -11,45 +12,35 @@
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     </head>
     <body>
-           <!-- BEGIN BANNER -->
-        <div style ="background-color:black;">
-            <img src="../media/assets/images/tripOutLogo.jpg"></img>
-            <div class ="pull-right" style="padding-top:20px;color:white;">
-                <div class="btn-group">
-                        <a href="signUp.php" class="btn btn-primary" id="registerButton">
-                            Register
-                        </a>
-                        <a href="signIn.php" class="btn btn-primary" id="registerButton">
-                            Sign In
-                        </a>
-                </div>    
-            </div>
-        </div>
+        <div class="container">
             <nav class="navbar navbar-inverse" role="navigation">
+                <a class="navbar-brand" href="../index.php">TRIP OUT!</a>
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="../index.php">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="reviewSearch.php">Write a Review</a></li>
+                    <li><a href="about.html">About</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Write a Review</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
-            </nav>
-            <!-- END BANNER -->
-    <div class="container">
-        <div class="row">
-            <div class="span5">
-                <h3>Sign In</h3>
-                <p>
-                <form name="logon" action="index.php" method="POST" >
-                    Username: <input type="text" name="user"><br>
-                    Password: <input type="password" name="userpassword"><br>
-
-                    <input class="btn btn-primary" type="submit" id="editButton" value="Sign In">
+                <form class="navbar-form navbar-right">
+                    <a type="submit" class="btn btn-default" href="signIn.php">Sign In</a>
+                    <a type="submit" class="btn btn-default" href="signUp.php">Register</a>
                 </form>
-                </p>
+            </nav>
+            <div class="row">
+                <div class="span5">
+                    <h3>Sign In</h3>
+                    <p>
+                    <form name="logon" action="index.php" method="POST" >
+                        Username: <input type="text" name="user"><br>
+                        Password: <input type="password" name="userpassword"><br>
+
+                        <input class="btn btn-primary" type="submit" id="editButton" value="Sign In">
+                    </form>
+                    </p>
                 </div>
                 <div class ="span5">
-                <h3>Haven't Tripped Out?</h3>
+                    <h3>Haven't Tripped Out?</h3>
                     <div class="btn-group">
                         <a href="signUp.php" class="btn btn-primary" id="registerButton">
                             Register Now
@@ -57,6 +48,11 @@
                     </div>
                 </div>
             </div>
+            <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+                <div class ="footer">
+                    SFSU-FAU-FULDA joint SW Engineering Project Fall 2013
+                </div>
+            </nav>
         </div>
     </body>
 </html>
--- Views/signUp.php
+++ Views/signUp.php
@@ -5,77 +5,79 @@
         <!-- <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css"> -->
         <link rel ="stylesheet" type ="text/css" href ="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
         <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css">
+        <link rel ="stylesheet" type ="text/css" href ="../css/index.css">
         <script src="../js/bootstrap.js"></script>
         <script src="../js/script.js"></script>
         <script src="../js/jquery.js"></script>
         <!-- Add jQuery library -->
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     </head>  
-    <body>   
-           <!-- BEGIN BANNER -->
-        <div style ="background-color:black;">
-            <img src="../media/assets/images/tripOutLogo.jpg"></img>
-            <div class ="pull-right" style="padding-top:20px;color:white;">
-                <div class="btn-group">
-                        <a href="signUp.php" class="btn btn-primary" id="registerButton">
-                            Register
-                        </a>
-                        <a href="signIn.php" class="btn btn-primary" id="registerButton">
-                            Sign In
-                        </a>
-                </div>    
-            </div>
-        </div>
+    <body> 
+        <!-- BEGIN CONTAINER -->
+        <div class="container">
+            <!-- BEGIN BANNER -->
             <nav class="navbar navbar-inverse" role="navigation">
+                <a class="navbar-brand" href="../index.php">TRIP OUT!</a>
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="../index.php">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="reviewSearch.php">Write a Review</a></li>
+                    <li><a href="about.html">About</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Write a Review</a></li>
                     <li><a href="#">FAQ</a></li>
                 </ul>
-            </nav>
- <!-- END BANNER -->       
- <div class="container">
-     <div class="row">
-         <div class="span6">
-             <div align="center" style="border:2px solid; border-radius:25px; display: block;
-               text-align: center; float: center; width: 50%; height: 50%">
-                <h3>Sign Up</h3>
-                <form class="form-horizontal" name="logon" action="index.php" method="POST" >
-                    <div class="control-group">
-                        <label class="control-label" for="inputUsername">Username:</label>
-                            <div class="controls">
-                                <input type="text" id="inputUsername" placeholder="username">
-                            </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="inputEmail">Email:</label>
-                            <div class="controls">
-                                <input type="text" id="inputEmail" placeholder="email">
-                            </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="inputPassword">Password:</label>
-                            <div class="controls">
-                                <input type="text" id="inputPassword" placeholder="password">
-                            </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="confirmPassword">Confirm Password:</label>
-                            <div class="controls">
-                                <input type="text" id="confirmpassword" placeholder="confirm password">
-                            </div>
-                    </div>
-                    <br>
-                    <a href="#" class="btn btn-primary" id="signUpButton">
-                    Register Now
-                    </a>
+                <form class="navbar-form navbar-right">
+                    <a type="submit" class="btn btn-default" href="signIn.php">Sign In</a>
+                    <a type="submit" class="btn btn-default" href="signUp.php">Register</a>
                 </form>
-         </div>
-         </div>
-     </div>
-</div>
+            </nav>
+            <!-- END BANNER -->       
+            <div class="row">
+                <div class="span6">
+                    <div align="center" style="border:2px solid; border-radius:25px; display: block;
+                      text-align: center; float: center; width: 50%; height: 50%">
+                       <h3>Sign Up</h3>
+                       <form class="form-horizontal" name="logon" action="index.php" method="POST" >
+                           <div class="control-group">
+                               <label class="control-label" for="inputUsername">Username:</label>
+                                   <div class="controls">
+                                       <input type="text" id="inputUsername" placeholder="username">
+                                   </div>
+                           </div>
+                           <div class="control-group">
+                               <label class="control-label" for="inputEmail">Email:</label>
+                                   <div class="controls">
+                                       <input type="text" id="inputEmail" placeholder="email">
+                                   </div>
+                           </div>
+                           <div class="control-group">
+                               <label class="control-label" for="inputPassword">Password:</label>
+                                   <div class="controls">
+                                       <input type="text" id="inputPassword" placeholder="password">
+                                   </div>
+                           </div>
+                           <div class="control-group">
+                               <label class="control-label" for="confirmPassword">Confirm Password:</label>
+                                   <div class="controls">
+                                       <input type="text" id="confirmpassword" placeholder="confirm password">
+                                   </div>
+                           </div>
+                           <br>
+                           <a href="#" class="btn btn-primary" id="signUpButton">
+                           Register Now
+                           </a>
+                       </form>
+                    </div>
+                </div>
+            </div>
+            <!-- BEGIN FOOTER-->
+            <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+                <div class ="footer">
+                    SFSU-FAU-FULDA joint SW Engineering Project Fall 2013
+                </div>
+            </nav>
+            <!-- END FOOTER -->
+        </div>
+        <!-- END CONTAINER-->
     </body>
 </html>
        
