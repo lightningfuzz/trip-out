830e0f5c4eb172030914dee9426517a22e639441
mdiamo <mdiamo@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-17 22:09:12 +0000

    formatted search parameters from index.php
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@184 6123cefa-f26d-4250-b61a-f760f7026737

--- index.php
+++ index.php
@@ -28,37 +28,40 @@
                     <a type="submit" class="btn btn-default" href="Views/signUp.php">Register</a>
                 </form>
             </nav>
-            <div class ="overview">
-                <h1>Welcome to Trip Out!</h1>
-                Plan your trip, read reviews, share your experiences
-            </div>
-            <div class="panel panel-default" style="width:75%;margin-left:auto;margin-right:auto;">
-                <div class="panel-heading">Plan Your Trip</div>
-                <div class="panel-body" align="center">
-                    <form action="Views/searchResult.php" method="post" class="form-inline" role="form">
-                        <div style="padding-bottom:10px;">
-                            <label class="checkbox-inline">
-                                <input type="radio" id="inlineCheckbox5" name = "searchType" value="option5"> All
-                            </label>
-                            <label class="checkbox-inline">
-                                <input type="radio" id="inlineCheckbox1" name ="searchType" value="option1"> Attractions
-                            </label>
-                            <label class="checkbox-inline">
-                                <input type="radio" id="inlineCheckbox2" name ="searchType" value="option2"> Restaurants
-                            </label>
-                            <label class="checkbox-inline">
-                                <input type="radio" id="inlineCheckbox3" name = "searchType" value="option3"> Hotels
+            <div class ="mainContent">
+                <div class ="overview">
+                    <h1>Welcome to Trip Out!</h1>
+                    Plan your trip, read reviews, share your experiences
+                </div>
+            
+                <div class="panel panel-default" style="width:75%;margin-left:auto;margin-right:auto;vertical-align:middle">
+                    <div class="panel-heading">Plan Your Trip</div>
+                    <div class="panel-body" align="center">
+                        <form action="Views/searchResult.php" method="get" class="form-inline" role="form">
+                            <div style="padding-bottom:10px;">
+                                <label class="checkbox-inline">
+                                    <input type="radio" id="inlineCheckbox5" name = "type" value="0"> All
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="radio" id="inlineCheckbox1" name ="type" value="1"> Attractions
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="radio" id="inlineCheckbox2" name ="type" value="2"> Restaurants
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="radio" id="inlineCheckbox3" name = "type" value="3"> Hotels
 
-                            </label>
-                            <label class="checkbox-inline">
-                                <input type="radio" id="inlineCheckbox4" name = "searchType" value="option4"> Events
-                            </label>
-                        </div>
-                        <div class="form-group" style="width:40%;">
-                          <input name="txtSearch" class="form-control"  placeholder="Enter search keywords" value="">
-                        </div>
-                        <button type="submit" class="btn btn-default">Search</button>
-                    </form>
+                                </label>
+                                <label class="checkbox-inline">
+                                    <input type="radio" id="inlineCheckbox4" name = "type" value="4"> Events
+                                </label>
+                            </div>
+                            <div class="form-group" style="width:40%;">
+                              <input name="searchString" class="form-control"  placeholder="Enter search keywords" value="">
+                            </div>
+                            <button type="submit" class="btn btn-default">Search</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
