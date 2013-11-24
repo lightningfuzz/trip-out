162bdad5470b2aee762f1a308a85ea50ee4dd081
jkim <jkim@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-24 04:29:04 +0000

    The updates has been posted: - class files except keyword and tag added on model directory.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@224 6123cefa-f26d-4250-b61a-f760f7026737

--- DAOs/ImageDAO.php
+++ DAOs/ImageDAO.php
@@ -11,7 +11,15 @@
  * @author Jung Hwan Kim
  */
 class ImageDAO {
+    
+    private function __construct(){
+    }
 
+    /**
+     * creates a new user account; returns a new user id.
+     * @param $user is the new image object
+     * @return the $image object with a new user id; return null if failed
+     */
     public static function create($image) {
         $db = dbConnet::getInstance();
         $q = "INSERT INTO review (user_id, dest_id, relUrl, "
@@ -23,29 +31,36 @@ class ImageDAO {
 
         if (!$db->query($q))
             return null;
-        $user->setUserID($db->insert_id);
+       
 
-        return $user;
+        return $image;
     }
 
+    /**
+     * searches the registered_user table by id
+     * @param $id the user id
+     * @return a Image object if found; else return null
+     */
     public static function getByDestId($integer) {
-         $db = dbConnect::getInstance();
+        $db = dbConnect::getInstance();
         //query
-        $q = "SELECT * FROM registered_user WHERE user_id =" . $id;
+        $q = "SELECT * FROM registered_user WHERE user_id =" . $integer;
 
-        if (!$result = $db->query($q)) return null;
-        
-        $arr = mysqli_fetch_array($result);
-        $ru = new RegisteredUser();
-        $ru->setUserID($arr["user_id"]);
-        $ru->setUserName($arr["user_name"]);
-        $ru->setPassword($arr["password"]);
-        $ru->setRegTime($arr["reg_time"]);
-        $ru->setLastLoginTime($arr["last_login_time"]);
-        $ru->setEmail($arr["email"]);
-        $ru->setNumReviews($arr["num_reviews"]);
-        
-        return $ru;
+        if (!$result = $db->query($q))
+            return null;
+        $images = array();
+        while($arr = mysqli_fetch_array($result)) {
+            $image = new Image();
+            $image->setUserId($arr["user_id"]);
+            $image->setDestId($arr["dest_id"]);
+            $image->setRelUrl($arr["relUrl"]);
+            $image->setTitle($arr["title"]);
+            $image->setDescription($arr["description"]);
+            $image->setUploadTime($arr["uploadTime"]);
+            $images[] = $image;
+        }
+
+        return $images;
     }
 
 }
--- DAOs/VideoDAO.php
+++ DAOs/VideoDAO.php
@@ -12,24 +12,52 @@
  */
 class VideoDAO {
 
+    /**
+     * creates a new user account; returns a new user id.
+     * @param $user is the new image object
+     * @return the $video object with a new user id; return null if failed
+     */
     public static function create() {
-        $db = dbConnect::getInstance();
-        //query
-        $q = "INSERT INTO registered_user (user_name, password, reg_time, "
-                . "last_login_time, email, num_reviews) VALUES ('"
-                . $user->getUserName() . "', '" . $user->getPassword() . "', '"
-                . $user->getRegTime() . "', '" . $user->getLastLoginTime() . "', '"
-                . $user->getEmail() . "', '" . $user->getNumReviews() . "')";
+        $db = dbConnet::getInstance();
+        $q = "INSERT INTO review (user_id, dest_id, relUrl, "
+                . "title, description, uploadTime,) VALUES ('"
+                . $video->getUserId() . "', '" . $video->getDestId() . "', '"
+                . $video->getRelUrl() . "', '" . $video>getTitle() . "', '"
+                . $video->getDescription() . "', '" . $video->getUploadTime() . "')";
+
 
         if (!$db->query($q))
             return null;
-        $user->setUserID($db->insert_id);
+       
 
-        return $user;
+        return $video;
     }
-
+    /**
+     * searches the registered_user table by id
+     * @param $id the user id
+     * @return a Video object if found; else return null
+     */
     public static function getByDestId() {
-        
+        $db = dbConnect::getInstance();
+        //query
+        $q = "SELECT * FROM registered_user WHERE user_id =" . $integer;
+
+        if (!$result = $db->query($q))
+            return null;
+
+        $videos = array();
+        while ($arr = mysqli_fetch_array($result)) {
+            $video = new Image();
+            $video->setUserId($arr["user_id"]);
+            $video->setDestId($arr["dest_id"]);
+            $video->setRelUrl($arr["relUrl"]);
+            $video->setTitle($arr["title"]);
+            $video->setDescription($arr["description"]);
+            $video->setUploadTime($arr["uploadTime"]);
+            $videos = $video;
+        }
+
+        return $videos;
     }
 
 }
