8b2241082025567b3beff3ace7ae27ebbd59293b
jkim <jkim@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-24 03:56:50 +0000

    The updates has been posted: - class files except keyword and tag added on model directory.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@223 6123cefa-f26d-4250-b61a-f760f7026737

--- DAOs/ImageDAO.php
+++ DAOs/ImageDAO.php
@@ -11,7 +11,43 @@
  * @author Jung Hwan Kim
  */
 class ImageDAO {
-    
+
+    public static function create($image) {
+        $db = dbConnet::getInstance();
+        $q = "INSERT INTO review (user_id, dest_id, relUrl, "
+                . "title, description, uploadTime,) VALUES ('"
+                . $image->getUserId() . "', '" . $image->getDestId() . "', '"
+                . $image->getRelUrl() . "', '" . $image->getTitle() . "', '"
+                . $image->getDescription() . "', '" . $image->getUploadTime() . "')";
+
+
+        if (!$db->query($q))
+            return null;
+        $user->setUserID($db->insert_id);
+
+        return $user;
+    }
+
+    public static function getByDestId($integer) {
+         $db = dbConnect::getInstance();
+        //query
+        $q = "SELECT * FROM registered_user WHERE user_id =" . $id;
+
+        if (!$result = $db->query($q)) return null;
+        
+        $arr = mysqli_fetch_array($result);
+        $ru = new RegisteredUser();
+        $ru->setUserID($arr["user_id"]);
+        $ru->setUserName($arr["user_name"]);
+        $ru->setPassword($arr["password"]);
+        $ru->setRegTime($arr["reg_time"]);
+        $ru->setLastLoginTime($arr["last_login_time"]);
+        $ru->setEmail($arr["email"]);
+        $ru->setNumReviews($arr["num_reviews"]);
+        
+        return $ru;
+    }
+
 }
 
 ?>
--- DAOs/VideoDAO.php
+++ DAOs/VideoDAO.php
@@ -11,7 +11,27 @@
  * @author Jung Hwan Kim
  */
 class VideoDAO {
-    //put your code here
+
+    public static function create() {
+        $db = dbConnect::getInstance();
+        //query
+        $q = "INSERT INTO registered_user (user_name, password, reg_time, "
+                . "last_login_time, email, num_reviews) VALUES ('"
+                . $user->getUserName() . "', '" . $user->getPassword() . "', '"
+                . $user->getRegTime() . "', '" . $user->getLastLoginTime() . "', '"
+                . $user->getEmail() . "', '" . $user->getNumReviews() . "')";
+
+        if (!$db->query($q))
+            return null;
+        $user->setUserID($db->insert_id);
+
+        return $user;
+    }
+
+    public static function getByDestId() {
+        
+    }
+
 }
 
 ?>
