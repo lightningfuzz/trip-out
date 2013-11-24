a71e201d4a8978c521ce00edf058bc068fc60011
jkim <jkim@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-24 03:17:25 +0000

    The updates has been posted: - class files except keyword and tag added on model directory.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@222 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ DAOs/ImageDAO.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ImageDAO
+ *
+ * @author Jung Hwan Kim
+ */
+class ImageDAO {
+    
+}
+
+?>
---
+++ DAOs/VideoDAO.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of VideoDAO
+ *
+ * @author Jung Hwan Kim
+ */
+class VideoDAO {
+    //put your code here
+}
+
+?>
