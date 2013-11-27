a0d0fefbde7814d54cd29a3bf16ff444ebb33dd9
jkim <jkim@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-27 03:34:56 +0000

    The updates has been posted: - class files except keyword and tag added on model directory.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@240 6123cefa-f26d-4250-b61a-f760f7026737

---
+++ Controllers/mediaController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of mediaController
+ *
+ * @author Jung Hwan Kim
+ */
+
+require_once '../Models/Image.php';
+require_once '../Models/Video.php';
+require_once '../DAOs/ImageDAO.php';
+require_once '../DAOs/VideoDAO.php';
+
+class mediaController {
+    
+   
+}
+
+?>
