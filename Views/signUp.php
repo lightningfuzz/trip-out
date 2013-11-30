ab7c641396225ab96d254334de8e989b69ff406b
rpenno <rpenno@6123cefa-f26d-4250-b61a-f760f7026737>
2013-11-30 01:12:58 +0000

    "signUp now working correctly complete with validity checks on user input. Validy checks done with html 5 and javascript"
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@288 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/signUp.php
+++ Views/signUp.php
@@ -2,11 +2,13 @@
     require_once("../Controllers/AccountController.php");
     require_once("../Models/RegisteredUser.php");
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $pass1=$_POST['InputPassword'];
+        $pass2=$_POST['ConfirmPassword'];
         $user = new RegisteredUser();
-        $user->setUserName($_POST['inputUsername']); //getting from the POST
-        $user->setEmail($_POST['inputEmail']);
-        $user->setPassword($_POST['inputPassword']);
-
+        $user->setUserName($_POST['InputUsername']); //getting from the POST
+        $user->setEmail($_POST['InputEmail']);
+        $user->setPassword($_POST['InputPassword']);
+        
         try{
             $result = AccountController::register($user);
          }
@@ -17,42 +19,102 @@
         header('Location: ../index.php');
         exit;
     }?>
-<<!-- Borrowed from Marcians original signup page
-<div class="row" align="center">
-    <!-- <div class="span6"> -->
+<html>
+    <head>
+         <!--<link rel ="stylesheet" type ="text/css" href ="../css/bootstrap.css"> -->
+        <link rel ="stylesheet" type ="text/css" href ="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+        <link rel ="stylesheet" type ="text/css" href ="../css/bootstrap-responsive.css">
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/script.js"></script>
+        <script src="../js/jquery.js"></script>
+        <!-- Add jQuery library -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <title></title>
+    </head>
+    <body>
+        
+        <!--script below checks to see if passwords match -->
+        <!-- logic courtesy of
+             http://www.sitepoint.com/using-the-html5-constraint-api-for-form-validation/-->
+
+        <script type="text/javascript">
+            window.onload = function () {
+               document.getElementById("InputPassword").onchange = validatePassword;
+               document.getElementById("ConfirmPassword").onchange = validatePassword;
+            }
+            function validatePassword(){
+            var pass2=document.getElementById("ConfirmPassword").value;
+            var pass1=document.getElementById("InputPassword").value;
+            if (pass1.length < 6 )
+                document.getElementById("InputPassword").setCustomValidity("Password must be at least 6 characters long");
+            else
+                document.getElementById("InputPassword").setCustomValidity(''); 
+                //clear pass.length check
+            if(pass1!=pass2)
+               document.getElementById("ConfirmPassword").setCustomValidity("Passwords Don't Match");
+            else
+               document.getElementById("ConfirmPassword").setCustomValidity('');  
+            //empty string means no validation error
+            }
+        </script>
+
+    <!-- Borrowed from Marcians original signup page
+
+    <div class="row" align="center">
+        <!-- <div class="span6"> -->
+
+
+        <nav class="navbar navbar-inverse" role="navigation">
+            <a class="navbar-brand" href="" id ="logo">TRIP OUT!</a>
+            <ul class="nav navbar-nav">
+                <li name = "home" class="active"><a href="">Home</a></li>
+                <li><a href="reviewSearch.php">Write a Review</a></li>
+                <li><a href="createDestination.php">Create a Destination</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <a type="submit" class="btn btn-default" href="Views/signIn.php" id ="signInButton">Sign In</a>
+                <a type="submit" class="btn btn-default" href="Views/signUp.php" id ="registerButton">Register</a>
+            </form>
+        </nav>
         <div align="center" style="border:2px solid; border-radius:25px; display: block;
         text-align: center; float: center; width: 30%; height: 65%">
          <h3>Sign Up</h3>
          <!-- form needed for sending POST -->
-         <form class="form-horizontal" action="signUp.php" method="POST" >
+         <form name="ValidationForm" class="form-horizontal" action="signUp.php" method="POST" >
              <div class="control-group">
-                 <label class="control-label" for="inputUsername">Username:</label>
+                 <label class="control-label" for="InputUsername">Username:</label>
                      <div class="controls">
-                         <input type="text" name="inputUsername" placeholder="username" required>
+                         <input type="text" name="InputUsername" id="InputUsername" placeholder="username" required>
                      </div>
              </div>
              <div class="control-group">
-                 <label class="control-label" for="inputEmail">Email:</label>
+                 <label class="control-label" for="InputEmail">Email:</label>
                      <div class="controls">
-                         <input type="text" name="inputEmail" placeholder="email" required>
+                         <input type="email" name="InputEmail" id="InputEmail" placeholder="email" required>
                      </div>
              </div>
              <div class="control-group">
-                 <label class="control-label" for="inputPassword">Password:</label>
+                 <label class="control-label" for="InputPassword">Password:</label>
                      <div class="controls">
-                         <input type="password" name="inputPassword" placeholder="password" required>
+                         <input type="password" name="InputPassword" id="InputPassword" placeholder="password" required>
                      </div>
              </div>
              <div class="control-group">
-                 <label class="control-label" for="confirmPassword">Confirm Password:</label>
+                 <label class="control-label" for="ConfirmPassword">Confirm Password:</label>
                      <div class="controls">
-                         <input type="password" name="confirmpassword" placeholder="confirm password" required>
+                         <input type="password" name="ConfirmPassword" id="ConfirmPassword" placeholder="confirm password" required>
                      </div>
              </div>
-             <br>
-             <input class ="btn btn-primary" type="submit"/>
-        </form>
-    </div>
-</div>
-            
+                 <!--script below checks to see if passwords match -->
+                 <!-- logic courtesy of
+                      http://www.sitepoint.com/using-the-html5-constraint-api-for-form-validation/-->
 
+                 <br>
+                 <input class ="btn btn-primary" type="submit"/>
+            </form>
+        </div>
+    </body>            
+</html>
