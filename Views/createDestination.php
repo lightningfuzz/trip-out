4359f65573d577c42b4e6fa71d6e1a594be8e070
msante <msante@6123cefa-f26d-4250-b61a-f760f7026737>
2013-12-02 19:00:49 +0000

    createDestination redirects properly now.
    git-svn-id: http://sfsuswe.com/svn/f13g05/m3@308 6123cefa-f26d-4250-b61a-f760f7026737

--- Views/createDestination.php
+++ Views/createDestination.php
@@ -1,7 +1,3 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <?php
     require_once("../Controllers/DestinationController.php");
     require_once("../Models/Destination.php");
@@ -25,7 +21,7 @@ and open the template in the editor.
             exit;
         }
         $destinationId=$result->getDestId();
-        header('Location: destinationDetail.php?' . $destinationId);
+        header('Location: destinationDetail.php?destinationId=' . $destinationId);
         exit;
     }?>
 <!DOCTYPE html>
